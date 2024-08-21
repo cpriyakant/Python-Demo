6b76c415bd9abb2229e1332d48fa675d792cedc2
cpriyakant <52397021+cpriyakant@users.noreply.github.com>
2024-08-21 21:20:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     name = request.args.get('name')
-    return f"Hello, {name}!"  # Vulnerable to XSS
+    return f"Hello, {name}!"  # Vulnerable to XSS Test 1
 
 if __name__ == '__main__':
     app.run(debug=True)
